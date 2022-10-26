3e1cd23d41458030f26e8d9038eab1a853dddd16
juannobl <jpnorena@sura.com.co>
2022-10-25 22:55:23 -0500

    se agrega implementacion palindromo

---
+++ src/main/java/com/ejercicios/testmeli/TestMeliPalindromo.java
@@ -0,0 +1,23 @@
+package com.ejercicios.testmeli;
+
+public class TestMeliPalindromo {
+
+    public static void main(String[] args) {
+
+        String[] cadenas = { "La ruta natural", "Esto no es", "Parzibyte", "Hola", "Sol", "Ana", "Oro", "Oso",
+                "A ti no, bonita",
+                "Adivina ya te opina, ya ni miles origina, ya ni cetro me domina, ya ni monarcas, a repaso ni mulato carreta, acaso nicotina, ya ni cita vecino, anima cocina, pedazo gallina, cedazo terso nos retoza de canilla goza, de pánico camina, ónice vaticina, ya ni tocino saca, a terracota luminosa pera, sacra nómina y ánimo de mortecina, ya ni giros elimina, ya ni poeta, ya ni vida",
+                "A mamá, Roma le aviva el amor a papá y a papá, Roma le aviva el amor a Mamá", "Me gusta programar en Java" };
+        for (String cadena : cadenas) {
+            System.out.println("¿'" + cadena + "' es palíndromo? " + esPalindromo(cadena));
+        }
+
+    }
+    public static boolean esPalindromo(String cadena) {
+        cadena = cadena.toLowerCase().replace("á", "a").replace("é", "e").replace("í", "i").replace("ó", "o")
+                .replace("ú", "u").replace(" ", "").replace(".", "").replace(",", "");
+        String inverted = new StringBuilder(cadena).reverse().toString();
+        return inverted.equals(cadena);
+    }
+
+}
